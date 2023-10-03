0489eafd5613250d98560c8b275567404a39d601
Hippot <hippot02@gmail.com>
2023-10-03 14:24:02 +0200

    Push de sauvegarde

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -8,7 +8,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -30,7 +29,7 @@ class UserCrudController extends AbstractCrudController
             TextField::new('password')
                 ->onlyWhenCreating()
                 ->setFormType(PasswordType::class),
-            ArrayField::new('roles'),
+            ArrayField::new('roles')
 
 
         ];
