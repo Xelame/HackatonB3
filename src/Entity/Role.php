86314a8b39af632400e416c4635bd91548ada748
Mael2231 <maelleroux.gjeg@gmail.com>
2023-10-03 09:46:19 +0200

    Creation des entitées

---
+++ src/Entity/Role.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoleRepository::class)]
+class Role
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Label = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->Label;
+    }
+
+    public function setLabel(string $Label): static
+    {
+        $this->Label = $Label;
+
+        return $this;
+    }
+}
