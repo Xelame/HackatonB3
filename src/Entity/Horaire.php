7cbe0f2c974770820915d628535a84d5fa332b65
Mael2231 <maelleroux.gjeg@gmail.com>
2023-10-05 14:27:13 +0200

    Recup l'état des salles toute les 5 min

--- src/Entity/Horaire.php
+++ src/Entity/Horaire.php
@@ -15,7 +15,7 @@ class Horaire
     private ?int $id = null;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
-    #[ORM\JoinColumn(name: 'idUser', referencedColumnName: 'etat_id')]
+    #[ORM\JoinColumn(name: 'idUser', referencedColumnName: 'user_id')]
     private ?User $user = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
